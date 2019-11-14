41e51a7d3b3c930485fbf05d6e87e9f647b9a807
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-14 16:05:36 +0200

    add login and logout functionality.

--- flaskr.py
+++ flaskr.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, \
+    url_for, flash
 
 DATABASE = ''
-USERNAME = ''
-PASSWORD = ''
-SECRET_KEY = ''
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'change me'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-    text = "Hello, World!"
-    return render_template('index.html', text=text)
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
